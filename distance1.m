d041342195b16c8ad16b6da0c31661748358ff44
denglingyun123 <59501302+denglingyun123@users.noreply.github.com>
2023-03-16 21:01:26 +0800

    Update distance1.m

--- distance1.m
+++ distance1.m
@@ -1,38 +1,5 @@
 function d = distance(a,b)
 % DISTANCE - computes Euclidean distance matrix
-%
-% E = distance(A,B)
-%
-%    A - (DxM) matrix 
-%    B - (DxN) matrix
-%
-% Returns:
-%    E - (MxN) Euclidean distances between vectors in A and B
-%
-%
-% Description : 
-%    This fully vectorized (VERY FAST!) m-file computes the 
-%    Euclidean distance between two vectors by:
-%
-%                 ||A-B|| = sqrt ( ||A||^2 + ||B||^2 - 2*A.B )
-%
-% Example : 
-%    A = rand(400,100); B = rand(400,200);
-%    d = distance(A,B);
-
-% Author   : Roland Bunschoten
-%            University of Amsterdam
-%            Intelligent Autonomous Systems (IAS) group
-%            Kruislaan 403  1098 SJ Amsterdam
-%            tel.(+31)20-5257524
-%            bunschot@wins.uva.nl
-% Last Rev : Oct 29 16:35:48 MET DST 1999
-% Tested   : PC Matlab v5.2 and Solaris Matlab v5.3
-% Thanx    : Nikos Vlassis
-
-% Copyright notice: You are free to modify, extend and distribute 
-%    this code granted that the author of the original code is 
-%    mentioned as the original author of the code.
 
 if (nargin ~= 2)
    error('Not enough input arguments');
