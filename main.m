2f3476bb0b97c433675f418d6a5d693bf6514957
denglingyun123 <59501302+denglingyun123@users.noreply.github.com>
2023-03-21 13:50:23 +0800

    Update main.m

--- main.m
+++ main.m
@@ -11,13 +11,10 @@ Max_iteration=1000; % Maximum number of iterations
 Max_test=30;
 for i=1:Max_test
     disp(['The ',num2str(i),'-th experiment']);
-    [Best_pos1(i,:),Best_score1(i),GOA_curve(i,:)]=GOA(SearchAgents_no,Max_iteration,lb,ub,dim,fobj); 
     [Best_pos2(i,:),Best_score2(i),HGOA_curve(i,:)]=HGOA(SearchAgents_no,Max_iteration,lb,ub,dim,fobj);
 end
 
 figure
-semilogy(mean(GOA_curve),'color','b','linewidth',2.0,'Marker','p','MarkerIndices',1:100:length(mean(GOA_curve)))
-hold on
 semilogy(mean(HGOA_curve),'color','[0.62745,0.12549,0.94118]','linewidth',2.0,'Marker','o','MarkerIndices',1:100:length(mean(HGOA_curve)))
 title('Convergence curve of F_{1}')
 xlabel('Iteration');
@@ -25,13 +22,8 @@ ylabel('Fitness');
 axis tight
 grid off
 box on 
-legend('GOA','HGOA')
+legend('HGOA')
 
-disp('-------------------------------------------------')
-display(['The best fitness value obtained by GOA over 30 independent runs : ', num2str(min(Best_score1))]);
-display(['The mean fitness value obtained by GOA over 30 independent runs: ', num2str(mean(Best_score1))]);
-display(['The worst fitness value obtained by GOA over 30 independent runs : ', num2str(max(Best_score1))]);
-display(['The standard deviation obtained by GOA over 30 independent runs : ', num2str(std(Best_score1))]);
 
 disp('-------------------------------------------------')
 display(['The best fitness value obtained by HGOA over 30 independent runs : ', num2str(min(Best_score2))]);
